e4f2850216a1100ae2396e1878ea01b6bf6a710b
Israel Roldan <israel.alberto.rv@gmail.com>
2024-02-12 15:07:36 -0600

    Updated manifest with new options ui; Updated the popup files.

--- src/ui/popup/main.js
+++ src/ui/popup/main.js
@@ -1,3 +1,30 @@
 // let title = browser.i18n.getMessage("extensionActionTitle");
 // let h1Element = document.querySelector("h1");
 // h1Element.innerHTML = title;
+
+document.querySelector("#url").innerHTML = `
+<pre>Page URL: ${window.location.href}</pre>
+`;
+
+document.querySelector("#xid").innerHTML = `
+<pre>Extension ID: ${browser.runtime.id}</pre>
+`;
+
+function onOpened() {
+    console.log(`Options page opened`);
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+document.querySelector("#opts").addEventListener(
+    "click",
+    () => {
+        //browser.runtime.openOptionsPage()
+        console.log("Click on options button in the PopUp.")
+        let opening = browser.runtime.openOptionsPage();
+        console.log(opening)
+        opening.then(onOpened, onError);
+    }
+);
